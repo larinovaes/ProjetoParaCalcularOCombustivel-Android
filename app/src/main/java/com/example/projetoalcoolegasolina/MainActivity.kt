e7afb03c8f5e5055af59fd0b1709d3e55288c527
Larissa Novais <larinovais54@gmail.com>
2022-03-16 13:02:56 -0300

    Criar função para calcular gasolina e alcool

--- app/src/main/java/com/example/projetoalcoolegasolina/MainActivity.kt
+++ app/src/main/java/com/example/projetoalcoolegasolina/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.projetoalcoolegasolina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+    }
+
+    private fun calculoDeGasolinaEAlcool(): String {
+        val gasolina = findViewById<EditText>(R.id.activity_gasolina)
+        val alcool = findViewById<EditText>(R.id.activity_alcool)
+
+        val melhorCombustivel = gasolina.text.toString().toDouble() * 0.70
+        val converterAlcool = alcool.text.toString().toDouble()
+
+        return if (melhorCombustivel < converterAlcool) {
+            "Gasolina é melhor"
+        } else {
+            "Alcool é melhor"
         }
     }
+}
